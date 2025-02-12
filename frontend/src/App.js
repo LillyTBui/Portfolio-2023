edfc52a6a6904fb0e536cb6f827fcbd89c165c7f
LillyTBui <lillythi1998@hotmail.com>
2025-02-12 04:11:10 +0100

    remove unused code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 import Projects from "./components/Projects/Projects";
-import ProjectHolidaze from "./components/Projects/ProjectHolidaze";
-import ProjectDuckSurfer from "./components/Projects/ProjectDuckSurfer";
-import ProjectHappyTourist from "./components/Projects/ProjectHappyTourist";
-import ProjectMuseum from "./components/Projects/ProjectMuseum";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
 import ScrollManager from "./ScrollManager";
@@ -21,16 +17,6 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/projects/:slug" element={<Project />} />
-          {/* <Route path="/projects/holidaze" element={<ProjectHolidaze />} />
-          <Route path="/projects/duckSurfer" element={<ProjectDuckSurfer />} />
-          <Route
-            path="/projects/happyTourist"
-            element={<ProjectHappyTourist />}
-          />
-          <Route
-            path="/projects/communityScienceMuseum"
-            element={<ProjectMuseum />}
-          /> */}
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </main>
--- frontend/src/components/Projects/Projects.jsx
+++ frontend/src/components/Projects/Projects.jsx
@@ -2,17 +2,6 @@ import { HashLink as Link } from "react-router-hash-link";
 import Container from "react-bootstrap/Container";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
-
-/* Images */
-import duckSurferSmall from "../../assets/duck_surfer_small.jpg";
-import duckSurferMedium from "../../assets/duck_surfer_medium.jpg";
-import duckSurfer from "../../assets/duck_surfer_big.jpg";
-import happyTouristSmall from "../../assets/happyTourist_small.jpg";
-import happyTouristMedium from "../../assets/happyTourist_medium.jpg";
-import happyTouristBig from "../../assets/happyTourist_big.jpg";
-import museumSmall from "../../assets/museum_small.jpg";
-import museumMedium from "../../assets/museum_medium.jpg";
-import museum from "../../assets/museum_big.jpg";
 import { useEffect, useState } from "react";
 import { PortableText } from "@portabletext/react";
 import { urlFor } from "../../SanityClient";
@@ -72,90 +61,6 @@ function Projects() {
               </div>
             </div>
           ))}
-          <div className="project__item">
-            <Link to="/projects/duckSurfer/#duckSurfer">
-              <img
-                srcSet={`${duckSurferSmall} 500w, ${duckSurferMedium} 1000w, ${duckSurfer} 1500w`}
-                src={duckSurferMedium}
-                alt="Duck surfer homepage"
-                className="project__item_img"
-              />
-            </Link>
-            <div className="project__item_text">
-              <h3 className="project__item_title">Duck Surfer</h3>
-              <p className="project__item_year">November 2021</p>
-              <p className="project__item_text-description">
-                Gruppeprosjekt i INFT1003 på NTNU. Oppgaven var å lage et spill,
-                og lære å samarbeide sammen med andre.
-              </p>
-              <Link
-                to="/projects/duckSurfer/#duckSurfer"
-                className="project__item-btn"
-              >
-                <p className="project__item-btn-text">Les mer</p>
-                <FontAwesomeIcon
-                  icon={faArrowRight}
-                  className="project__item-btn-icon"
-                />
-              </Link>
-            </div>
-          </div>
-          <div className="project__item">
-            <Link to="/projects/happyTourist/#happyTourist">
-              <img
-                srcSet={`${happyTouristSmall} 500w, ${happyTouristMedium} 1000w, ${happyTouristBig} 1500w`}
-                src={happyTouristMedium}
-                alt="The Happy Tourist homepage"
-                className="project__item_img"
-              />
-            </Link>
-            <div className="project__item_text">
-              <h3 className="project__item_title">The Happy Tourist</h3>
-              <p className="project__item_year">Oktober 2021</p>
-              <p className="project__item_text-description">
-                Eksamensprosjekt 1 på Noroff. Oppgaven var å lage en blogg med
-                valgfritt tema.
-              </p>
-              <Link
-                to="/projects/happyTourist/#happyTourist"
-                className="project__item-btn"
-              >
-                <p className="project__item-btn-text">Les mer</p>
-                <FontAwesomeIcon
-                  icon={faArrowRight}
-                  className="project__item-btn-icon"
-                />
-              </Link>
-            </div>
-          </div>
-          <div className="project__item">
-            <Link to="/projects/CommunityScienceMuseum/#museum">
-              <img
-                srcSet={`${museumSmall} 500w, ${museumMedium} 1000w, ${museum} 1500w`}
-                src={museum}
-                alt="Community Science Museum homepage"
-                className="project__item_img"
-              />
-            </Link>
-            <div className="project__item_text">
-              <h3 className="project__item_title">Community Science Museum</h3>
-              <p className="project__item_year">Februar 2021</p>
-              <p className="project__item_text-description">
-                Semesterprosjekt 1 på Noroff. Oppgave var å lage en nettside for
-                et fiktivt museum.
-              </p>
-              <Link
-                to="/projects/CommunityScienceMuseum/#museum"
-                className="project__item-btn"
-              >
-                <p className="project__item-btn-text">Les mer</p>
-                <FontAwesomeIcon
-                  icon={faArrowRight}
-                  className="project__item-btn-icon"
-                />
-              </Link>
-            </div>
-          </div>
         </div>
       </div>
     </Container>
